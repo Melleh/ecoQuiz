5524976725e437f01f8891960f741eb367c14231
melvin <melvinboelens@gmail.com>
2021-01-11 10:54:59 +0100

    BG structure

---
+++ views/home.view.php
@@ -0,0 +1,21 @@
+<div class="home-page">
+    <header>
+        <br>
+        <h1>Welcome to EcoQuiz!</h1>
+        <hr>
+    </header>
+    <div class="page-content">
+        <p>
+            Take the EcoQuiz now, like tens of people already did before you!
+            <br>
+        </p>
+        <button>
+            Lets Go!
+        </button>
+        <blockquote class="blockquote">
+            <p>60&#37; of the time, its a 100&#37; accurate!</p>
+            <footer class="blockquote-footer">Some guy</footer>
+        </blockquote>
+
+    </div>
+</div>
